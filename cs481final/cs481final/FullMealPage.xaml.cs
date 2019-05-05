adcada553213312f33833aab652cb5781f3603d6
ClayFlores <ClayFlores@users.noreply.github.com>
2019-05-04 23:32:42 -0700

    minimum viable product

--- cs481final/cs481final/FullMealPage.xaml.cs
+++ cs481final/cs481final/FullMealPage.xaml.cs
@@ -21,17 +21,18 @@ namespace cs481final
         {
             InitializeComponent();
 
-            img.Source = meal.strMealThumb;
-            title.Text = meal.strMeal;
-            ing1.Text = meal.strIngredient1;
-            ing2.Text = meal.strIngredient2;
-            ing3.Text = meal.strIngredient3;
-            ing4.Text = meal.strIngredient4;
-            ing5.Text = meal.strIngredient5;
-            ing6.Text = meal.strIngredient6;
-            ing7.Text = meal.strIngredient7;
-            ing8.Text = meal.strIngredient8;
-            ing9.Text = meal.strIngredient9;
+            BindingContext = meal;
+            //img.Source = meal.strMealThumb;
+            //title.Text = meal.strMeal;
+            //ing1.Text = meal.strIngredient1;
+            //ing2.Text = meal.strIngredient2;
+            //ing3.Text = meal.strIngredient3;
+            //ing4.Text = meal.strIngredient4;
+            //ing5.Text = meal.strIngredient5;
+            //ing6.Text = meal.strIngredient6;
+            //ing7.Text = meal.strIngredient7;
+            //ing8.Text = meal.strIngredient8;
+            //ing9.Text = meal.strIngredient9;
             //ing10.Text = meal.strIngredient10;
             //ing11.Text = meal.strIngredient11;
             //ing12.Text = meal.strIngredient12;
@@ -41,15 +42,15 @@ namespace cs481final
 
 
 
-            mea1.Text = meal.strMeasure1;
-            mea2.Text = meal.strMeasure2;
-            mea3.Text = meal.strMeasure3;
-            mea4.Text = meal.strMeasure4;
-            mea5.Text = meal.strMeasure5;
-            mea6.Text = meal.strMeasure6;
-            mea7.Text = meal.strMeasure7;
-            mea8.Text = meal.strMeasure8;
-            mea9.Text = meal.strMeasure9;
+            //mea1.Text = meal.strMeasure1;
+            //mea2.Text = meal.strMeasure2;
+            //mea3.Text = meal.strMeasure3;
+            //mea4.Text = meal.strMeasure4;
+            //mea5.Text = meal.strMeasure5;
+            //mea6.Text = meal.strMeasure6;
+            //mea7.Text = meal.strMeasure7;
+            //mea8.Text = meal.strMeasure8;
+            //mea9.Text = meal.strMeasure9;
             //mea10.Text = meal.strMeasure10;
             //mea11.Text = meal.strMeasure11;
             //mea12.Text = meal.strMeasure12;
@@ -57,7 +58,7 @@ namespace cs481final
             //mea14.Text = meal.strMeasure14;
             //mea15.Text = meal.strMeasure15;
 
-            instr.Text = meal.strInstructions;
+            //instr.Text = meal.strInstructions;
         }
 	}
 }
