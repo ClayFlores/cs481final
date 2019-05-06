899a82cb9cc1ee5fb51929a0e4976783fc0e5400
ClayFlores <ClayFlores@users.noreply.github.com>
2019-05-06 13:58:34 -0700

    removes redundant pages

--- cs481final/cs481final/startpage.xaml.cs
+++ cs481final/cs481final/startpage.xaml.cs
@@ -18,14 +18,22 @@ namespace cs481final
 		}
         private async void Food_Button_Clicked(object sender, EventArgs e)
         {
-            //await DisplayAlert("Food Button","Clicked!","Sounds Good", "cancel"); // reminder: CAN DO 3 ARG OR 4
-            await Navigation.PushAsync(new FoodPickPage());
-
+            if (string.IsNullOrWhiteSpace(itemToSearch.Text))
+            {
+                await DisplayAlert("Error", "Please enter a food item to search for", "Ok");
+            }
+            else
+                await Navigation.PushAsync(new RecipePage(itemToSearch.Text));
         }
 
         private async void Drink_Button_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new DrinkPickPage());
+            if (string.IsNullOrWhiteSpace(itemToSearch.Text))
+            {
+                await DisplayAlert("Error", "Please enter a drink to search for", "Ok");
+            }
+            else
+                await Navigation.PushAsync(new DrinkRecipePage(itemToSearch.Text));
         }
     }
 }
