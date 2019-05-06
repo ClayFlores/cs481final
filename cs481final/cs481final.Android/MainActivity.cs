a6c8c850aab3d6d44fc13eee62397736296e076d
ClayFlores <ClayFlores@users.noreply.github.com>
2019-05-05 17:01:48 -0700

    analytics

--- cs481final/cs481final.Android/MainActivity.cs
+++ cs481final/cs481final.Android/MainActivity.cs
@@ -7,6 +7,10 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
 namespace cs481final.Droid
 {
     [Activity(Label = "cs481final", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
@@ -19,7 +23,11 @@ namespace cs481final.Droid
 
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            AppCenter.Start("6f7cf46a-978f-4ff3-9072-e99617c94908",
+                   typeof(Analytics), typeof(Crashes));
+            AppCenter.Start("6f7cf46a-978f-4ff3-9072-e99617c94908", typeof(Analytics), typeof(Crashes));
             LoadApplication(new App());
+
         }
     }
 }
--- cs481final/cs481final.iOS/AppDelegate.cs
+++ cs481final/cs481final.iOS/AppDelegate.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using Foundation;
 using UIKit;
 
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
 namespace cs481final.iOS
 {
     // The UIApplicationDelegate for the application. This class is responsible for launching the 
@@ -24,7 +28,8 @@ namespace cs481final.iOS
         {
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
-
+            AppCenter.Start("3d84d0c8-367d-4c7e-941a-adc5af8a4a07",
+                   typeof(Analytics), typeof(Crashes));
             return base.FinishedLaunching(app, options);
         }
     }
